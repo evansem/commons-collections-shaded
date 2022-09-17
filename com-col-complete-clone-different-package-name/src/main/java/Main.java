f47ff1d322be3227a048490a78928734b9a4287f
Emanuel Evans <evanseman@ecs.vuw.ac.nz>
2022-09-18 10:37:41 +1200

    Add com col complete clone different package

---
+++ com-col-complete-clone-different-package-name/src/main/java/Main.java
@@ -0,0 +1,19 @@
+import clone.myorg.apache.commons.collections.Bag;
+import clone.myorg.apache.commons.collections.bag.HashBag;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Bag bag = new HashBag();
+        bag.add("Hello");
+        bag.add("World");
+        System.out.println(bag.size());
+
+//        Bag bag = new HashBag();
+//        Path path = Paths.get(args[0]);
+//        byte[] data = Files.readAllBytes(path);
+//        InputStream d = new ByteArrayInputStream(data);
+//        ObjectInputStream ois = new ObjectInputStream(d);
+//        ois.readObject();
+    }
+}
